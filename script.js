74ec84f5c65dbf7f0de6234ac55f8c333f6ce181
phjjj <102671646+phjjj@users.noreply.github.com>
2022-10-24 21:48:52 +0900

    Initial commit

---
+++ script.js
@@ -0,0 +1,56 @@
+const video = document.getElementById("video")
+
+Promise.all([
+  faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
+  faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+  faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
+  faceapi.nets.faceExpressionNet.loadFromUri('/models'),
+  faceapi.nets.ageGenderNet.loadFromUri('/models')
+]).then(startVideo)
+
+function startVideo() {
+  navigator.mediaDevices
+    .getUserMedia({ video: true })
+    .then(function (stream) {
+      video.srcObject = stream;
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
+}
+
+video.addEventListener('play', () => {
+  const canvas = faceapi.createCanvasFromMedia(video)
+  document.body.append(canvas)
+  const displaySize = { width: video.width, height: video.height}
+  faceapi.matchDimensions(canvas, displaySize)
+  setInterval(async () => {
+    const detections = await faceapi.detectAllFaces(video,
+    new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks
+    ().withFaceExpressions().withAgeAndGender()
+    console.log(detections)
+    const resizedDetections = faceapi.resizeResults(detections, displaySize)
+    canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
+    faceapi.draw.drawDetections(canvas, resizedDetections)
+    faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
+    faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
+
+    resizedDetections.forEach(result => {
+      const { age, gender, genderProbability } = result;
+      testAge = age
+    });
+    console.log(testAge)
+    
+    if (testAge >=60) {
+      changePage()
+    }
+
+    function changePage() {
+      location.href="index2.html"
+    }
+
+
+  }, 600)
+
+
+})
