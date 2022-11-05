ef618d905a8374e123e940efb52fb99804968e90
phjjj <102671646+phjjj@users.noreply.github.com>
2022-11-05 13:47:38 +0900

    a
    a

--- script.js
+++ script.js
@@ -21,7 +21,6 @@ function startVideo() {
 
 video.addEventListener('play', () => {
   const canvas = faceapi.createCanvasFromMedia(video)
-  document.body.append(canvas)
   const displaySize = { width: video.width, height: video.height}
   faceapi.matchDimensions(canvas, displaySize)
   setInterval(async () => {
