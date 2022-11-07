c85e85918967258d23fc0cbb2befa136b44779ee
phjjj <102671646+phjjj@users.noreply.github.com>
2022-11-08 00:42:56 +0900

    go
    go

---
+++ js/password.js
@@ -0,0 +1,8 @@
+const number = document.querySelectorAll(".number");
+for (var i = 0; i < number.length; i++) {
+  number[i].addEventListener("click", sibal);
+}
+
+function sibal() {
+  console.log("sex");
+}
