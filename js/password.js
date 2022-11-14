22fbf52f6ea8e09966cefece5dd7dc7ed99dabaa
phjjj <102671646+phjjj@users.noreply.github.com>
2022-11-14 17:39:45 +0900

    시발
    거래내역 조회

--- js/password.js
+++ js/password.js
@@ -3,9 +3,12 @@ const password = document.querySelector("input");
 
 for (var i = 0; i < number.length; i++) {
   number[i].addEventListener("click", sibal);
-    
-    function sibal() {
-      
+
+  function sibal() {
+    password.value = "1" + password.value;
+
+    if (password.value.toString().length == 4) {
+      location.href = "withdraw.html";
+    }
   }
 }
-
--- js/withdraw.js
+++ js/withdraw.js
@@ -3,8 +3,9 @@ const input = document.querySelector("input");
 
 for (var i = 0; i < money.length; i++) {
   money[i].addEventListener("click", sibal);
-
+  console.log(money[i].value);
   function sibal() {
     location.href = "withdraw-2.html";
+    input.value = 3;
   }
 }
