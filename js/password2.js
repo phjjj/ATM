bd75d69bf02fef31997e5ea16336760addb9f1f5
phjjj <102671646+phjjj@users.noreply.github.com>
2022-11-17 19:27:03 +0900

    20221117
    스크린+

--- js/password2.js
+++ js/password2.js
@@ -5,7 +5,7 @@ for (var i = 0; i < number.length; i++) {
   number[i].addEventListener("click", sibal);
   function sibal() {
     if (input.value.toString().length == 4) {
-      location.href = "withdraw.html";
+      location.href = "deposit-4.html";
     }
   }
 }
--- js/phonenumber.js
+++ js/phonenumber.js
@@ -4,8 +4,8 @@ const input = document.querySelector("input");
 for (var i = 0; i < number.length; i++) {
   number[i].addEventListener("click", sibal);
   function sibal() {
-    if (input.value.toString().length == 4) {
-      location.href = "withdraw.html";
+    if (input.value.toString().length == 11) {
+      location.href = "deposit-3.html";
     }
   }
 }
